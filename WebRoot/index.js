169db389d1d06826b94475af61367f109b9dcf00
unknown <yc04325@L4488101.federated.fds>
2015-09-21 12:57:39 -0700

    some modifications

--- WebRoot/index.js
+++ WebRoot/index.js
@@ -36,66 +36,6 @@ function config($stateProvider, $urlRouterProvider,$httpProvider){
 			templateUrl	:"modules/admin/users/Users.tpl.html",
 			controller	: UsersController
 		})
-		.state("contractor",{
-			url			:"/contractor",
-			templateUrl	:"modules/contractor/Contractor.tpl.html",
-			controller	: ContractorController
-		})
-		.state("contractor.home",{
-			url			:"/home",
-			templateUrl	:"modules/contractor/home/ContractorHome.tpl.html",
-			controller	: ContractorHomeController
-		})
-		.state("contractor.home.allprojects",{
-			url			:"/allprojects",
-			templateUrl	:"modules/contractor/home/allprojects/ContractorAllProjects.tpl.html",
-			controller	: ContractorAllProjectsController
-		})
-		.state("contractor.home.paymentinfo",{
-			url			:"/paymentinfo",
-			templateUrl	:"modules/contractor/home/paymentinfo/ContractorPaymentInfo.tpl.html",
-			controller	: ContractorPaymentInfoController
-		})
-		.state("contractor.home.reports",{
-			url			:"/reports",
-			templateUrl	:"modules/contractor/home/reports/ContractorReports.tpl.html",
-			controller	: ContractorReportsController
-		})
-		.state("contractor.home.settings",{
-			url			:"/settings",
-			templateUrl	:"modules/contractor/home/settings/ContractorSettings.tpl.html",
-			controller	: ContractorSettingsController
-		})
-		.state("contractor.home.mailbox",{
-			url			:"/mailbox",
-			templateUrl	:"modules/contractor/home/mailbox/ContractorMailbox.tpl.html",
-			controller	: ContractorMailboxController
-		})
-		.state("contractor.project",{
-			url			:"/project",
-			templateUrl	:"modules/contractor/project/ContractorProject.tpl.html",
-			controller	: ContractorProjectController
-		})
-		.state("supplier",{
-			url			:"/supplier",
-			templateUrl	:"modules/supplier/Supplier.tpl.html",
-			controller	: SupplierController
-		})
-		.state("supplier.machines",{
-			url			:"/machines",
-			templateUrl	:"modules/supplier/machines/Machines.tpl.html",
-			controller	: MachinesController
-		})
-		.state("supplier.chemicals",{
-			url			:"/chemicals",
-			templateUrl	:"modules/supplier/chemicals/Chemicals.tpl.html",
-			controller	: ChemicalsController
-		})
-		.state("supplier.toolsandconsumables",{
-			url			:"/toolsandconsumables",
-			templateUrl	:"modules/supplier/toolsandconsumables/ToolsAndConsumables.tpl.html",
-			controller	: ToolsAndConsumablesController
-		})
 		.state("mobilenotsuported",{
 			url			:"/mobilenotsupported",
 			templateUrl	:"templates/not_suported.tpl.html"
@@ -106,7 +46,7 @@ function config($stateProvider, $urlRouterProvider,$httpProvider){
 	
 }
 
-angular.module('eEstimateApp',['ngImgCrop','angucomplete','ui.router', 'ngAnimate','ngResource','context.service','login','rest.service','error.utils','ngCookies','xeditable','ui.bootstrap','model.service','ngTagsInput','cleanestimate.directives','cleanestimate.filters'])
+angular.module('eEstimateApp',['ngImgCrop','angucomplete','ui.router', 'ngAnimate','ngResource','context.service','login','rest.service','error.utils','ngCookies','xeditable','ui.bootstrap','model.service','ngTagsInput'])
 	.config(config)
 	.factory('myHttpInterceptor', function($q) {
 	    return {
