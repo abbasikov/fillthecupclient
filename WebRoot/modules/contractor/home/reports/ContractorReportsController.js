c28073739361142c878d63b5beff3e09f5cb6121
unknown <yc04325@L4488101.federated.fds>
2015-09-21 12:48:16 -0700

    deleted contractor

--- WebRoot/modules/contractor/home/reports/ContractorReportsController.js
+++
@@ -1,6 +0,0 @@
-function ContractorReportsController($scope,$state,Notification,context){
-	
-}
-
-angular.module('contractor.home.reports',['ngAnimate','ui.router','ui-notification','angularFileUpload'])
-	.controller('ContractorReportsController',['$scope','$state','Notification','context',ContractorReportsController]);
