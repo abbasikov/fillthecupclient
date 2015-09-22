c984da94577cbe5ae35210beec7cf833471340dd
unknown <yc04325@L4488101.federated.fds>
2015-09-22 10:34:59 -0700

    register controller changed

--- WebRoot/modules/register/RegisterController.js
+++ WebRoot/modules/register/RegisterController.js
@@ -1,6 +1,12 @@
 function RegisterController($scope,$state,Notification,FileUploader,RegisterService,ErrorUtils,CONSTANTS,context,$cookies,loadContext){
 	Notification.success({message: 'Please fill the form for new registration.', title: 'Welcome to FillTheCup'});
-		
+	
+	$scope.registerText = "SIGN UP";
+	    
+	//Add page-signup class in body tag 
+	angular.element('#id-body').removeClass('page-signin');
+	angular.element('#id-body').addClass('page-signup');
+			
 }
 angular.module('register',['ngAnimate','ui.router','ui-notification','angularFileUpload'])
 	.controller('RegisterController',['$scope','$state','Notification','FileUploader','RegisterService','ErrorUtils','CONSTANTS','context','$cookies','loadContext',RegisterController]);
